1a86a36181511cadb69b5b83df71161026d83eba
Michal Stanke <michal@stanke.cz>
2017-09-22 23:29:02 +0200

    Sanitize the link URL, fix #2

--- background.js
+++ background.js
@@ -3,7 +3,9 @@ browser.contextMenus.create({
     title: browser.i18n.getMessage('emailLinkContextMenu'),
     contexts: ['link'],
     onclick: (info) => {
-        browser.tabs.create({url: `mailto:?body=${info.linkUrl}`, active: false})
+        const mailtoUrl = new URL('mailto:');
+        mailtoUrl.searchParams.set('body', info.linkUrl);
+        browser.tabs.create({url: mailtoUrl.toString(), active: false})
             .then(tab => browser.tabs.remove(tab.id));
     },
 });

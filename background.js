861c8b9b9de565252ea885e5cd27e5632153d58a
Michal Stanke <michal@stanke.cz>
2017-11-26 18:18:58 +0100

    Email links to pages and frames, fix #5

--- background.js
+++ background.js
@@ -1,12 +1,28 @@
+function shareLink(url) {
+    const mailtoUrl = new URL('mailto:');
+    mailtoUrl.searchParams.set('body', url);
+    browser.tabs.create({url: mailtoUrl.toString(), active: false})
+        .then(tab => browser.tabs.remove(tab.id));
+}
+
 browser.contextMenus.create({
     type: 'normal',
     title: browser.i18n.getMessage('emailLinkContextMenu'),
     contexts: ['link'],
-    onclick: (info) => {
-        const mailtoUrl = new URL('mailto:');
-        mailtoUrl.searchParams.set('body', info.linkUrl);
-        browser.tabs.create({url: mailtoUrl.toString(), active: false})
-            .then(tab => browser.tabs.remove(tab.id));
-    },
+    onclick: (info) => shareLink(info.linkUrl),
+});
+
+browser.contextMenus.create({
+    type: 'normal',
+    title: browser.i18n.getMessage('emailPageLinkContextMenu'),
+    contexts: ['page'],
+    onclick: (info) => shareLink(info.pageUrl),
+});
+
+browser.contextMenus.create({
+    type: 'normal',
+    title: browser.i18n.getMessage('emailFrameLinkContextMenu'),
+    contexts: ['frame'],
+    onclick: (info) => shareLink(info.frameUrl),
 });
 

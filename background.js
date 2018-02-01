ea194b5235c7cfcdbb3c5c8393dbcc76b4634a7e
Michal Stanke <michal@stanke.cz>
2018-02-01 09:43:02 +0100

    Add support for elements possible, fix #6

--- background.js
+++ background.js
@@ -27,3 +27,30 @@ browser.contextMenus.create({
     onclick: (info) => shareLink(info.frameUrl),
 });
 
+browser.contextMenus.create({
+    type: 'normal',
+    title: browser.i18n.getMessage('emailImageLinkContextMenu'),
+    contexts: ['image'],
+    onclick: (info) => shareLink(info.srcUrl),
+});
+
+browser.contextMenus.create({
+    type: 'normal',
+    title: browser.i18n.getMessage('emailAudioLinkContextMenu'),
+    contexts: ['audio'],
+    onclick: (info) => shareLink(info.srcUrl),
+});
+
+browser.contextMenus.create({
+    type: 'normal',
+    title: browser.i18n.getMessage('emailVideoLinkContextMenu'),
+    contexts: ['video'],
+    onclick: (info) => shareLink(info.srcUrl),
+});
+
+browser.contextMenus.create({
+    type: 'normal',
+    title: browser.i18n.getMessage('emailTabLinkContextMenu'),
+    contexts: ['tab'],
+    onclick: (info) => shareLink(info.pageUrl),
+});
